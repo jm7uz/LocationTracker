621a01108310be53518ae0a7ad370b9ee7f98609
Jaloliddin Ganijonov <jm7uzdev@gmail.com>
2024-03-12 14:20:20 +0500

    ServiceExtentions implemented

---
+++ src/LocationTracker.Data/IRepositories/Districts/IDistrictRepository.cs
@@ -0,0 +1,7 @@
+﻿using LocationTracker.Domain.Entities.Districts;
+
+namespace LocationTracker.Data.IRepositories.Districts;
+
+public interface IDistrictRepository : IRepository<District, int>
+{
+}
---
+++ src/LocationTracker.Data/Repositories/Regions/RegionRepository.cs
@@ -0,0 +1,14 @@
+﻿using LocationTracker.Data.DbContexts;
+using LocationTracker.Data.IRepositories.Regions;
+using LocationTracker.Domain.Entities.Regions;
+
+namespace LocationTracker.Data.Repositories.Regions;
+
+public class RegionRepository : Repository<Region, int>, IRegionRepository
+{
+    public RegionRepository(LocationTrackerDbContext context) : 
+        base(context)
+    {
+
+    }
+}
