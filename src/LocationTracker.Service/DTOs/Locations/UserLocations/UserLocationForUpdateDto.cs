b98071d30f181e97b7061d466f3174b25e63dde8
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-03-13 13:51:39 +0500

    Create DTOs of UserLocation

---
+++ src/LocationTracker.Service/DTOs/Locations/UserLocations/UserLocationForUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LocationTracker.Service.DTOs.Locations.UserLocations;
+
+public class UserLocationForUpdateDto
+{
+    public long UserId { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public bool IsVerified { get; set; }
+}
