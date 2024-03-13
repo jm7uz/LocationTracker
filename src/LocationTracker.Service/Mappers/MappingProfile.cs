d25c90133c993a904f6ccc273abeb081e55b7629
Jaloliddin Ganijonov <jm7uzdev@gmail.com>
2024-03-13 13:48:00 +0500

    AttachedAreaService interface implemented.

--- src/LocationTracker.Service/Mappers/MappingProfile.cs
+++ src/LocationTracker.Service/Mappers/MappingProfile.cs
@@ -1,7 +1,16 @@
 ﻿using AutoMapper;
+using LocationTracker.Domain.Entities.Locations;
+using LocationTracker.Service.DTOs.Locations.AttachedAreas;
 namespace LocationTracker.Service.Mappers;
 
 public class MappingProfile : Profile
 {
-
+    
+    public MappingProfile()
+    {
+        //AttachedArea
+        CreateMap<AttachedArea, AttachedAreaForCreationDto>();
+        CreateMap<AttachedArea, AttachedAreaForResultDto>();
+        CreateMap<AttachedArea, AttachedAreaForUpdateDto>();
+    }
 }
