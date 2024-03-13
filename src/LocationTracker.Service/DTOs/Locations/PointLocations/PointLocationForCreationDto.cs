87a5b6f247d89fe3859f497f0dd0609e0fb402ce
Muhammadziyo Mahkamov <127393000+Ziyobek617@users.noreply.github.com>
2024-03-13 13:51:02 +0500

    Create DTOs of PointLocation

---
+++ src/LocationTracker.Service/DTOs/Locations/PointLocations/PointLocationForCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LocationTracker.Service.DTOs.Locations.PointLocations;
+
+public class PointLocationForCreationDto
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public int AttachedAreaId { get; set; }
+}
