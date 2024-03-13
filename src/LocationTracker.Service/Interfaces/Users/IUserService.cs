e8031789903db7232032ad2e32678ca3e05243b8
AbdumajidovAvazbek <abdumajidova51@gmail.com>
2024-03-13 15:26:42 +0500

    Added: IUserService

---
+++ src/LocationTracker.Service/Interfaces/Users/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using LocationTracker.Service.DTOs.Users;
+using LocationTracker.Service.Configurations;
+
+namespace LocationTracker.Service.Interfaces.Users;
+
+public interface IUserService
+{
+    Task<bool> RemoveAsync(int id);
+    Task<UserForResultDto> RetrieveByIdAsync(int id);
+    Task<UserForResultDto> CreateAsync(UserForCreationDto dto);
+    Task<UserForResultDto> ModifyAsync(long id, UserForUpdateDto dto);
+    Task<IEnumerable<UserForResultDto>> RetrieveAllAsync(PaginationParams @params);
+    Task<UserForResultDto> ModifyPhoneNumberAsync(long id, UserPhoneNumberModifyDto dto);
+    Task<UserForResultDto> ModifyAttachAreaAsync(long id, UserAttachAreaModifyDto dto);
+    Task<UserForResultDto> ModifyRoleAsync(long id, UserRoleModifyDto dto);
+
+}
