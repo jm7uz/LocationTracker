c0388f104de53a576b7d2d02301638ee573f07ae
Dilafruza <dilafruzjraboyeva@gmail.com>
2024-03-15 15:25:49 +0500

    Some bugs are fixed

--- src/LocationTracker.Service/DTOs/Locations/UserLocations/UserLocationForCreationDto.cs
+++ src/LocationTracker.Service/DTOs/Locations/UserLocations/UserLocationForCreationDto.cs
@@ -5,5 +5,4 @@ public class UserLocationForCreationDto
     public long UserId { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
-    public bool IsVerified { get; set; }
 }
